42966c7e162f5fe0c038dceb2cb00d7832b98e3e
Marcelo Linhares Castro <marlinhares@gmail.com>
2020-03-10 10:55:51 -0300

    Ajustes testes

--- testes/SeleniumIDE/exportedPython/test_030SEIModWsseiSEI.py
+++ testes/SeleniumIDE/exportedPython/test_030SEIModWsseiSEI.py
@@ -53,6 +53,7 @@ class Test030SEIModWsseiSEI():
     dropdown = self.driver.find_element(By.ID, "selInfraUnidades")
     dropdown.find_element(By.XPATH, "//option[. = 'TESTE_1_2']").click()
     for i in range(0, 40):
+      WebDriverWait(self.driver, 30000).until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "Acompanhamento Especial")))
       self.driver.find_element(By.LINK_TEXT, "Acompanhamento Especial").click()
       WebDriverWait(self.driver, 30000).until(expected_conditions.visibility_of_element_located((By.ID, "btnGrupo")))
       self.driver.find_element(By.ID, "btnGrupo").click()
@@ -79,7 +80,7 @@ class Test030SEIModWsseiSEI():
       self.driver.find_element(By.ID, "btnAdicionar").click()
       self.vars["data"] = self.driver.execute_script("var d= new Date(); var m=((d.getMonth()+1)<10)?\'0\'+(d.getMonth()+1):(d.getMonth()+1); return d.getFullYear()+\'-\'+m+\'-\'+d.getDate();")
       self.vars["hora"] = self.driver.execute_script("return (new Date().getHours()+\'-\' + new Date().getMinutes() + \'-\' + new Date().getSeconds())")
-      self.driver.find_element(By.ID, "txtCargoFuncao").send_keys("Cargo " + self.vars["data"] + "-" + self.vars["hora"])
+      self.driver.find_element(By.ID, "txtCargoFuncao").send_keys("Cargo "+ self.vars["data"] + "-" + self.vars["hora"])
       self.driver.find_element(By.ID, "txtUnidade").click()
       WebDriverWait(self.driver, 30000).until(expected_conditions.element_to_be_clickable((By.ID, "txtUnidade")))
       self.driver.find_element(By.ID, "txtUnidade").send_keys("teste")
