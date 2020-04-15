b14b0594fbd3ef995438b75093870de00763a6dd
eduardoromao <abcd1234>
2020-04-15 14:23:10 -0300

    [FEAT] Adicionando atributos e informações na lista de companhamento especial.

--- rn/MdWsSeiAcompanhamentoRN.php
+++ rn/MdWsSeiAcompanhamentoRN.php
@@ -178,7 +178,80 @@ class MdWsSeiAcompanhamentoRN extends InfraRN
             /** Acessando componente SEI para retorna da lista de acompanhamentos da unidade **/
             $arrAcompanhamentoDTO = $acompanhamentoRN->listarAcompanhamentosUnidade($acompanhamentoDTOConsulta);
 
+            $bolAcaoRegistrarAnotacao = SessaoSEI::getInstance()->verificarPermissao('anotacao_registrar');
+
             foreach($arrAcompanhamentoDTO as $acompanhamentoDTO) {
+                $arrAnotacao = array();
+                $arrRetornoProgramado = array();
+                $arrAndamentoSituacao = array();
+                $arrAndamentoMarcador = array();
+
+                $procedimentoDTO = $acompanhamentoDTO->getObjProcedimentoDTO();
+                $anotacaoDTO = $procedimentoDTO->getObjAnotacaoDTO();
+                $andamentoSituacaoDTO = $procedimentoDTO->getObjAndamentoSituacaoDTO();
+                $andamentoMarcadorDTO = $procedimentoDTO->getObjAndamentoMarcadorDTO();
+
+
+
+                if($anotacaoDTO && $bolAcaoRegistrarAnotacao){
+                    $arrAnotacao = array(
+                        'descricao' => $anotacaoDTO->getStrDescricao(),
+                        'siglaUsuario' => $anotacaoDTO->getStrSiglaUsuario(),
+                        'prioridade' => $anotacaoDTO->getStrSinPrioridade() == 'N' ? false : true
+                    );
+                }
+
+                if($procedimentoDTO->getArrObjRetornoProgramadoDTO() != null){
+                    $arrRetornoProgramado['atrasado'] = false;
+                    $strDataAtual = InfraData::getStrDataAtual();
+
+                    foreach($procedimentoDTO->getArrObjRetornoProgramadoDTO() as $retornoProgramadoDTO) {
+                        $descricaoPrazo = '';
+                        $numPrazo = InfraData::compararDatas($strDataAtual,$retornoProgramadoDTO->getDtaProgramada());
+
+                        if ($numPrazo < 0){
+                            $arrRetornoProgramado['atrasado'] = true;
+                        }
+
+                        if ($numPrazo==0){
+                            $descricaoPrazo .= 'até hoje';
+                        }else if ($numPrazo==1){
+                            $descricaoPrazo .= '1 dia';
+                        }else if ($numPrazo > 1){
+                            $descricaoPrazo .= $numPrazo.' dias';
+                        }else if ($numPrazo == -1){
+                            $descricaoPrazo .= 'atrasado 1 dia';
+                        }else if ($numPrazo < -1){
+                            $descricaoPrazo .= 'atrasado '.abs($numPrazo).' dias';
+                        }
+
+                        $arrRetornoProgramado['lista'][] = array(
+                            'siglaUnidade' => $retornoProgramadoDTO->getStrSiglaUnidadeOrigemAtividadeEnvio(),
+                            'dataProgramada' => $retornoProgramadoDTO->getDtaProgramada(),
+                            'descriaoPrazo' => $descricaoPrazo
+                        );
+                    }
+                }
+
+                if($andamentoSituacaoDTO){
+                    $arrAndamentoSituacao = array(
+                        'situacao' => SituacaoINT::formatarSituacaoDesativada(
+                            $andamentoSituacaoDTO->getStrNomeSituacao(),
+                            $andamentoSituacaoDTO->getStrSinAtivoSituacao()
+                        ),
+                        'gerenciarSituacao' => SessaoSEI::getInstance()->verificarPermissao('andamento_situacao_gerenciar')
+                    );
+                }
+                if($andamentoMarcadorDTO){
+                    $arrAndamentoMarcador = array(
+                        'marcador' => MarcadorINT::formatarMarcadorDesativado(
+                            $andamentoMarcadorDTO->getStrNomeMarcador(),
+                            $andamentoMarcadorDTO->getStrSinAtivoMarcador()
+                        ),
+                        'gerenciarMarcador' => SessaoSEI::getInstance()->verificarPermissao('andamento_marcador_gerenciar')
+                    );
+                }
+
                 $result[] = array(
                     'idAcompanhamento' => $acompanhamentoDTO->getNumIdAcompanhamento(),
                     'idGrupoAcompanhamento' => $acompanhamentoDTO->getNumIdGrupoAcompanhamento(),
@@ -193,6 +266,17 @@ class MdWsSeiAcompanhamentoRN extends InfraRN
                     'tipoVisualizacao' => $acompanhamentoDTO->getNumTipoVisualizacao(),
                     'nomeTipoProcedimento' => $acompanhamentoDTO->getObjProcedimentoDTO()->getStrNomeTipoProcedimento(),
                     'protocoloFormatado' => $acompanhamentoDTO->getObjProcedimentoDTO()->getStrProtocoloProcedimentoFormatado(),
+                    'atributos' => array(
+                        'anotacao' => $arrAnotacao,
+                        'processoBloquado' => $procedimentoDTO->getStrStaEstadoProtocolo() == ProtocoloRN::$TE_PROCEDIMENTO_BLOQUEADO ? true : false,
+                        'remocaoSobrestamento' => ($acompanhamentoDTO->getNumTipoVisualizacao() == AtividadeRN::$TV_REMOCAO_SOBRESTAMENTO) ? true : false,
+                        'documentoAssinadoProcesso' => ($acompanhamentoDTO->getNumTipoVisualizacao() == AtividadeRN::$TV_ATENCAO) ? true : false,
+                        'documentoPublicadoProcesso' => ($acompanhamentoDTO->getNumTipoVisualizacao() == AtividadeRN::$TV_PUBLICACAO) ? true : false,
+                        'retornoProgramado' => $arrRetornoProgramado,
+                        'andamentoSituacao' => $arrAndamentoSituacao,
+                        'andamentoMarcador' => $arrAndamentoMarcador,
+
+                    )
                 );
             }
 
